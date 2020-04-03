518201b23b94ee0f0038d91a0f4da09402676d64
Patrick <pgalligan@rockarch.org>
2020-04-03 11:56:40 -0400

    Add models
    adding model back.

--- request_broker/settings.py
+++ request_broker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'process_request'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'process_request.MachineUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
