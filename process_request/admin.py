cc938b80fef63b467b2ee06e239c97bc0917e343
Hillel Arnold <helrond@hotmail.com>
2022-09-26 15:48:06 -0400

    add User admin

--- process_request/admin.py
+++ process_request/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ConfigList, ConfigPair
+from .models import ConfigList, ConfigPair, User
 
 
 class ConfigPairInline(admin.TabularInline):
@@ -11,3 +11,8 @@ class ConfigPairInline(admin.TabularInline):
 @admin.register(ConfigList)
 class ConfigListAdmin(admin.ModelAdmin):
     inlines = [ConfigPairInline]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
